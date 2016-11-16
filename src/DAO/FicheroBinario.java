5d070f25b09d26b00ff27895140ec60abfdcc28b
jesus <jesus@Anonymous>
2016-11-16 12:31:31 +0100

    v.0.0.1

--- src/DAO/FicheroBinario.java
+++ src/DAO/FicheroBinario.java
@@ -9,12 +9,8 @@ package DAO;
 
 import gestionDeMovimientos.modelo.Empleado;
 import gestionDeMovimientos.modelo.Movimiento;
-import java.io.DataInputStream;
 import java.io.DataOutputStream;
-import java.io.EOFException;
 import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.ArrayList;
@@ -44,34 +40,19 @@ public class FicheroBinario
         }
     }
 
-       public void escribirFicherosDatEmpleado(String numeroDeEmpleado, String nombre,
-            String apellido) throws IOException {
-        /**
-         * Declaramos todos los Ficheros. con el mkdir creamos el directorio.
-         * Con el createNewFile creamos el Fichero
-         */
+    public void escribirFicherosDatEmpleado(String numeroDeEmpleado, String nombre,
+        String apellido) throws IOException {
         File archivoDatEmpleados = new File("./Empleado.dat");
         FileOutputStream fos;
-
         if (!archivoDatEmpleados.exists()) {
             archivoDatEmpleados.createNewFile();
         }
-        /**
-         * DataOutputStream guarda como ficheros de datos. llama a la clase con
-         * la que escribiremos en un fichero binario.
-         */
         fos = new FileOutputStream("./Empleado.dat", true);
         DataOutputStream dos = new DataOutputStream(fos);
-        /**
-         * Con .write escribiremos en el fichero, hay que recordar que se tiene
-         * que acompañar con el tipo de dato que escribamos, UTF si es String,
-         * Int para enteros, etc. Aqui escribiremos lo que queremos que escriba
-         * (guarde) en el fichero:
-         */
+        
         dos.writeUTF(numeroDeEmpleado);
         dos.writeUTF(nombre);
         dos.writeUTF(apellido);
-        dos.close(); //Cerramos la clase printWriter
-        
+        dos.close(); 
     }
 }
